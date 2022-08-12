8f86036fb9c6fb7567e62d0f9333da04367a157d
Muhaddis <muhaddisrehman12@gmail.com>
2022-08-12 11:37:26 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Cards from './components/Cards';
+// import Cards from './components/Cards';
+// import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
-  <>
-    
-<Header/>
-<Cards title="ththth"/>
-  </>     
+      <div>
+        <Header/>
+      </div>
 );
 }
 
